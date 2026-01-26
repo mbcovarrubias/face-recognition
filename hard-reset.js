aeebccb2b572d145ecac49fb49d123e36e73a5ea
mbcovarrubias <mbcovarrubias@feudiliman.edu.ph>
2026-01-27 05:51:12 +0800

    Update hard-reset.js

--- hard-reset.js
+++ hard-reset.js
@@ -14,7 +14,7 @@ function multiQuery(db,queryList,cb) {
 	}
 }
 
-connection.connect(err=>{
+connection.connect(async(err)=>{
 	//clear images
 	let folderPath = path.join(__dirname,"public","images")
 	fs.rmSync(folderPath, {recursive: true, force: true});
@@ -27,6 +27,7 @@ connection.connect(err=>{
 		"CREATE DATABASE `face-recognition`",
 		"USE `face-recognition`",
 		"CREATE TABLE RegisteredUsers (name VARCHAR(255) UNIQUE, email VARCHAR(255) UNIQUE)",
+		"CREATE TABLE FaceRecognitionResults (accuracy DECIMAL(5,2))",
 		"CREATE TABLE Archive (date VARCHAR(255) UNIQUE)",
 	],(idx,err,res)=>{
 		if (err) console.error(err);
